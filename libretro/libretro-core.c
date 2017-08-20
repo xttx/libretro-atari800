9ae5626de1d2db2fee73b64df41bd200b84906bb
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2017-08-19 20:27:55 -0500

    Add ATX awareness to the libretro core code.

--- libretro/libretro-core.c
+++ libretro/libretro-core.c
@@ -363,7 +363,7 @@ void retro_get_system_info(struct retro_system_info *info)
    memset(info, 0, sizeof(*info));
    info->library_name     = "Atari800";
    info->library_version  = "3.1.0" GIT_VERSION;
-   info->valid_extensions = "xfd|atr|cdm|cas|bin|a52|zip";
+   info->valid_extensions = "xfd|atr|cdm|cas|bin|a52|zip|atx";
    info->need_fullpath    = true;
    info->block_extract = false;
 
