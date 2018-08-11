aa9b7a305a9422b3a20160e37a63c1383e7522e1
Sven <40953353+RetroSven@users.noreply.github.com>
2018-08-11 11:37:09 -0400

    add memory hooks for cheat/rumble

--- libretro/libretro-core.c
+++ libretro/libretro-core.c
@@ -742,13 +742,15 @@ bool retro_unserialize(const void *data_, size_t size)
 
 void *retro_get_memory_data(unsigned id)
 {
-   (void)id;
+  if ( id == RETRO_MEMORY_SYSTEM_RAM )
+    return MEMORY_mem;
    return NULL;
 }
 
 size_t retro_get_memory_size(unsigned id)
 {
-   (void)id;
+  if ( id == RETRO_MEMORY_SYSTEM_RAM )
+    return 65536;
    return 0;
 }
 
